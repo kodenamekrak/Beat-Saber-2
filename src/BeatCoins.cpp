2b3317632741b6b152fbcde666eea4d8ef4f5962
kodenamekrak <kyelodge2005@gmail.com>
2022-08-05 21:19:28 +0100

    Added song requirement of 90s, UI looks bit better

--- src/BeatCoins.cpp
+++ src/BeatCoins.cpp
@@ -10,15 +10,21 @@
 #include "System/Threading/Tasks/Task_1.hpp"
 #include "GlobalNamespace/ScoreModel.hpp"
 #include "GlobalNamespace/IBeatmapDataBasicInfo.hpp"
+#include "UnityEngine/Application.hpp"
+#include "GlobalNamespace/GameSongController.hpp"
+
 
 using namespace GlobalNamespace;
 int GainedBeatCoins;
 bool triggered; 
-float percentage, maxScore, Score;
+float percentage, maxScore, Score, songLength;
+
+int scorePerBeatcoin = 800000;
 
 HMUI::ImageView *BeatCoinsImage;
 UnityEngine::GameObject *BeatCoinsCanvas;
 TMPro::TextMeshProUGUI *BeatCoinsCount;
+TMPro::TextMeshProUGUI *NextBeatCoinProgress;
 
 void calculateMaxScore(int blockCount)
 {
@@ -37,16 +43,28 @@ void calculateMaxScore(int blockCount)
     getLogger().info("maxScore for this song is %f", maxScore);
 }
 
+MAKE_AUTO_HOOK_MATCH(SongLengthGetter, &GameSongController::StartSong, void, GameSongController *self, float songTimeOffset)
+{
+    SongLengthGetter(self, songTimeOffset);
+
+    songLength = self->get_songLength();
+    getLogger().info("Song length is %f", songLength);
+}
+
 MAKE_AUTO_HOOK_MATCH(BeatCoinsDisplayUpdater, &GameplaySetupViewController::DidActivate, void, GameplaySetupViewController *self, bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling)
 {
     BeatCoinsDisplayUpdater(self, firstActivation, addedToHierarchy, screenSystemEnabling);
 
     if (firstActivation)
     {   // create beatcoins image and counter
-        BeatCoinsCanvas = QuestUI::BeatSaberUI::CreateCanvas();
-        BeatCoinsCanvas->get_transform()->set_position({2, 2.65, 4.3f});
-        BeatCoinsImage = QuestUI::BeatSaberUI::CreateImage(BeatCoinsCanvas->get_transform(), QuestUI::BeatSaberUI::Base64ToSprite(BeatCoinsLogo), {-35, 2}, {8, 8});
-        BeatCoinsCount = QuestUI::BeatSaberUI::CreateText(BeatCoinsCanvas->get_transform(), std::to_string(getModConfig().BeatCoinsCount.GetValue()));
+        ///BeatCoinsCanvas = QuestUI::BeatSaberUI::CreateCanvas();
+        //BeatCoinsCanvas->get_transform()->set_position({2, 2.65, 4.3f});
+        BeatCoinsImage = QuestUI::BeatSaberUI::CreateClickableImage(self->get_transform(), QuestUI::BeatSaberUI::Base64ToSprite(BeatCoinsLogo), {176, 47}, {8, 8}, [&]()
+        {
+            UnityEngine::Application::OpenURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+        });
+        BeatCoinsCount = QuestUI::BeatSaberUI::CreateText(self->get_transform(), std::to_string(getModConfig().BeatCoinsCount.GetValue()));
+        BeatCoinsCount->get_rectTransform()->set_anchoredPosition({210, 45});
         triggered = true;
     }
     BeatCoinsCount->SetText(std::to_string(getModConfig().BeatCoinsCount.GetValue()));
@@ -63,26 +81,18 @@ MAKE_AUTO_HOOK_MATCH(BeatCoinsResultsHider, &ResultsViewController::Init, void, 
         BeatCoinsImage->get_gameObject()->SetActive(false);
     }
     // get our score at the end of a song
-    Score = levelCompletionResults->modifiedScore + getModConfig().BeatCoinProgress.GetValue();
+    Score = levelCompletionResults->modifiedScore;
+    //songLength = levelCompletionResults->endSongTime;
     auto beatmapDataTask = difficultyBeatmap->GetBeatmapDataBasicInfoAsync();
     auto beatmapData = beatmapDataTask->get_Result();
 
     auto notesCount = beatmapData->get_cuttableNotesCount();
     calculateMaxScore(notesCount);
 
-    getLogger().info("Total Score was %f", Score);
-    // gives one beatcoin per 800,000 score
-    for (GainedBeatCoins = 0; Score >= 800000; GainedBeatCoins++)
-    {
-        Score = Score - 800000;
-    }
-    getModConfig().BeatCoinsCount.SetValue(getModConfig().BeatCoinsCount.GetValue() + GainedBeatCoins);
-    getLogger().info("%i BeatCoins were gained with %f score remaining", GainedBeatCoins, Score);
-    getModConfig().BeatCoinProgress.SetValue(Score);
-    GainedBeatCoins = 0;
-
     percentage = (Score / maxScore) * 100;
 
+    if(songLength >= 90.0f)
+    { 
     if(percentage >= 98.0)
     {
         GainedBeatCoins = 3;
@@ -94,10 +104,21 @@ MAKE_AUTO_HOOK_MATCH(BeatCoinsResultsHider, &ResultsViewController::Init, void, 
     else if (percentage >= 90.0)
     {
         GainedBeatCoins = 1;
-    }
-
+    }    
     getModConfig().BeatCoinsCount.SetValue(getModConfig().BeatCoinsCount.GetValue() + GainedBeatCoins);
     getLogger().info("%i BeatCoins were gained with a percentage of %f", GainedBeatCoins, percentage);
+
+    // gives one beatcoin per 800,000 score
+    Score = levelCompletionResults->modifiedScore + getModConfig().BeatCoinProgress.GetValue();
+    for (GainedBeatCoins = 0; Score >= scorePerBeatcoin; GainedBeatCoins++)
+    {
+        Score = Score - scorePerBeatcoin;
+    }
+    getModConfig().BeatCoinsCount.SetValue(getModConfig().BeatCoinsCount.GetValue() + GainedBeatCoins);
+    getLogger().info("%i BeatCoins were gained with %f score remaining", GainedBeatCoins, Score);
+    getLogger().info("Level score was %i, Config score is %i, Combined score is %f", levelCompletionResults->modifiedScore, getModConfig().BeatCoinProgress.GetValue(), Score);
+    getModConfig().BeatCoinProgress.SetValue(Score + getModConfig().BeatCoinProgress.GetValue());
+    }
 }
 
 MAKE_AUTO_HOOK_MATCH(BeatCoinsMainMenuHider, &MainMenuViewController::DidActivate, void, MainMenuViewController *self, bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling)
